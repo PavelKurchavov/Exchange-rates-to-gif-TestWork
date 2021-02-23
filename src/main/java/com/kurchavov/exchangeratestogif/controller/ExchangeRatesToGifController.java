f7d4e6b932beedbf10742b0f6df7cd65cef20949
Pavel Kurchavov <pavelkurchavov@gmail.com>
2021-02-23 23:08:57 +0300

    logic moved to service

--- src/main/java/com/kurchavov/exchangeratestogif/controller/ExchangeRatesToGifController.java
+++ src/main/java/com/kurchavov/exchangeratestogif/controller/ExchangeRatesToGifController.java
@@ -1,7 +1,8 @@
 package com.kurchavov.exchangeratestogif.controller;
 
-import com.kurchavov.exchangeratestogif.currency_rates_service.CurrencyRatesService;
-import com.kurchavov.exchangeratestogif.gif_service.GifService;
+
+import com.kurchavov.exchangeratestogif.exchangeratestogif_service.ExchangeRatesToGifService;
+import com.kurchavov.exchangeratestogif.exchangeratestogif_service.ExchangeRatesToGifServiceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -13,30 +14,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequiredArgsConstructor
 public class ExchangeRatesToGifController {
-    private final CurrencyRatesService currencyRatesService;
-    private final GifService gifService;
+    private final ExchangeRatesToGifService service;
 
     @Value("${oxr.base}")
     private String baseCurrency;
 
     @GetMapping("/get_gif")
     public String getRatesAndReturnGif(@RequestParam(value = "to", defaultValue = "EUR") String to, Model model) {
-        String linkToGif;
-        String htmlPage;
+            ExchangeRatesToGifServiceResponse response;
 
-        if(currencyRatesService.getExchangeRate(to) > 0) {
-            linkToGif = gifService.getRandomGif("rich");
-            htmlPage = "rich";
-        }
-        else {
-            linkToGif = gifService.getRandomGif("broke");
-            htmlPage = "broke";
-        }
+            try {
+              response = service.getResponse(to);
+            } catch (NullPointerException e) {
+                return "404notFound";
+            }
 
-        model.addAttribute("linkToGif", linkToGif);
-        model.addAttribute("baseCurrency", baseCurrency);
-        model.addAttribute("currencyTo", to);
+            String searchQuery = response.getSearchQuery();
+            model.addAttribute("linkToGif", response.getGifEmbedUrl());
+            model.addAttribute("baseCurrency", baseCurrency);
+            model.addAttribute("currencyTo", to);
 
-        return htmlPage;
+        return searchQuery;
     }
 }
